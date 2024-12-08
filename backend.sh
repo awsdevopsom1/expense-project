53fa455eb4c2b2a36d13552257ac2cf86c3884a2
om <awsdevopsom@gmail.com>
2024-12-08 20:13:51 +0530

    message

--- backend.sh
+++ backend.sh
@@ -8,19 +8,19 @@ MYSQL_ROOT_PASSWORD=$1
 
 echo -e "${colour} disable and enable nodejs \e[0m"
 dnf module disable nodejs -y  &>>$log_file
-status_check()
+status_check
 
 echo -e "${colour} enable nodejs \e[0m"
 dnf module enable nodejs -y  &>>$log_file
-status_check()
+status_check
 
 echo -e "${colour} install nodejs \e[0m"
 dnf install nodejs -y  &>>$log_file
-status_check()
+status_check
 
 echo -e "${colour} copy backend.service file \e[0m"
 cp backend.service /etc/systemd/system/backend.service &>>log_file
-status_check()
+status_check
 
 id expense &>>$log_file
 if [ $? -ne 0 ]; then
@@ -41,12 +41,12 @@ status_check
 
 echo -e "${colour} Download the backend \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip &>>log_file
-status_check()
+status_check
 
 echo -e "${colour} extract the app directory \e[0m"
 cd /app  &>>log_file
 unzip /tmp/backend.zip &>>log_file
-status_check()
+status_check
 
 echo -e "${color} Download NodeJS Dependencies \e[0m"
 npm install &>>$log_file
@@ -54,17 +54,17 @@ status_check
 
 echo -e "${colour} install mysql client \e[0m"
 dnf install mysql -y &>>log_file
-status_check()
+status_check
 
 echo -e "${colour} loadschema \e[0m"
 mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -p${MYSQL_ROOT_PASSWORD} < /app/schema/backend.sql &>>log_file
-status_check()
+status_check
 
 echo -e "${colour} enable & restart the backend service \e[0m"
 systemctl daemon-reload &>>log_file
 systemctl enable backend &>>log_file
 systemctl start backend &>>log_file
-status_check()
+status_check
 
 
 
--- mysql.sh
+++ mysql.sh
@@ -9,28 +9,28 @@ MYSQL_ROOT_PASSWORD=$1
 
 echo -e "${colour} disable mysql \e[0m"
 dnf module disable mysql -y &>>log_file
-status_check()
+status_check
 
 echo -e "${colour} copy the mysql.repo \e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>log_file
-status_check()
+status_check
 
 echo -e "${colour} installing the mysql community server \e[0m"
 dnf install mysql-community-server -y &>>$log_file
-status_check()
+status_check
 
 echo -e "${colour} installing the mysql community server \e[0m"
 dnf install mysql-community-server -y &>>$log_file
-status_check()
+status_check
 
 echo -e "${colour} reset mysql DB passowrd \e[0m"
 mysql_secure_installation --set-root-pass ${MYSQL_ROOT_PASSWORD} &>>$log_file
-status_check()
+status_check
 
 echo -e "${colour} start the service \e[0m"
 systemctl enable mysqld  &>>$log_file
 systemctl start mysqld  &>>$log_file
-status_check()
+status_check
 
 
 
