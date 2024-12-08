692e096f881ab3921753f0f9deb147f018a3def7
om <awsdevopsom@gmail.com>
2024-12-08 19:38:59 +0530

    message

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,19 @@
 source common.sh
 echo -e "${colour} installing the nginx \e[0m"
 dnf install nginx -y &>>$log_file
-status_check()
+status_check
 
 echo -e "${colour} copy expense config file \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
-status_check()
+status_check
 
 echo -e "${colour} remove the default content that webserver is having \e[0m"
 rm -rf /usr/share/nginx/html/* &>>$log_file
-status_check()
+status_check
 
 echo -e "${colour} download the frontend content \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip  &>>$log_file
-status_check()
+status_check
 
 echo -e "${colour} change directory & copy the content \e[0m"
 cd /usr/share/nginx/html  &>>$log_file
@@ -23,7 +23,7 @@ status_check
 echo -e "${colour}  enable & restart the nignx \e[0m"
 systemctl enable nginx &>>$log_file
 systemctl restart nginx &>>$log_file
-status_check()
+status_check
 
 
 
